86851456f074ed35156de240143d095a40231cbe
Hkashyap <hkashyap.developer@gmail.com>
2023-01-30 23:29:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import MainNav from '../src/Modules/Global/Header.jsx';
+import Footer from '../src/Modules/Global/Footer.jsx';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App fullPage">
+      <MainNav />
       <h2>test</h2>
+      <Footer />
     </div>
   );
 }
