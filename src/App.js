d18bb2ec10a0b18ed42ca3149a7704a14e39ee93
Hkashyap <hkashyap.developer@gmail.com>
2023-02-01 05:08:37 +0530

    router update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Router>
       <MainNav />        
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="" element={<Home/>}/>
           <Route exact path="/about" element={<About/>}/>
           <Route exact path="/services" element={<Services/>}/>
           <Route exact path="/contact" element={<Contact/>}/>                    
