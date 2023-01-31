9fb21cf3c94c79c1c357de24dada725a6d91a37b
Hkashyap <hkashyap.developer@gmail.com>
2023-01-31 21:15:35 +0530

    Updates

---
+++ precache-manifest.52e63cc09260c3201387bae026642eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df0c8158952f3d3c1bb2",
+    "url": "/mv3/static/css/main.cc7ce5df.chunk.css"
+  },
+  {
+    "revision": "df0c8158952f3d3c1bb2",
+    "url": "/mv3/static/js/main.df0c8158.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/mv3/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "9c26bddc2d526f09caf6",
+    "url": "/mv3/static/js/2.9c26bddc.chunk.js"
+  },
+  {
+    "revision": "b71988cbcb4165351468",
+    "url": "/mv3/static/js/runtime~main.b71988cb.js"
+  },
+  {
+    "revision": "9e3d8802338bd2ae95e1ff853da95d8e",
+    "url": "/mv3/index.html"
+  }
+];
