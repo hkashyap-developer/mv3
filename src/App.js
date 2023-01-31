fbc7923d5637779685a1455b35f63cc580b1f93f
Hkashyap <hkashyap.developer@gmail.com>
2023-02-01 05:15:29 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import About from '../src/Pages/About/About.jsx';
 import Services from '../src/Pages/Services/Services.jsx';
 import Contact from '../src/Pages/Contact/Contact.jsx';
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App fullPage">
 
-      <Router>
+      <Router basename="/mv3">
       <MainNav />        
         <Routes>
-          <Route exact path="" element={<Home/>}/>
+          <Route exact path="/" element={<Home/>}/>
           <Route exact path="/about" element={<About/>}/>
           <Route exact path="/services" element={<Services/>}/>
           <Route exact path="/contact" element={<Contact/>}/>                    
